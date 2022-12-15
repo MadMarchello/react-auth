a00b36debf03d68d8860f739fcad3b4b620cfbdb
Mark Belousov <mark1010_00@mail.ru>
2022-12-15 17:46:30 +0700

    add mui auth form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,38 @@
 import React, { useState, useContext} from 'react';
+
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+
 import { Context } from '../index';
 
+const Copyright = (props) => {
+    return (
+        <Typography 
+            variant="body2" 
+            color="text.secondary"
+            align="center"
+            {...props}
+        >
+            {'Copyright © '}
+            Платформа "Образование"
+            {' '}
+            {new Date().getFullYear()}.
+        </Typography>
+    )
+}
 const LoginForm = () => {
-    const [email, setEmail] = useState('test@test.ru');
-    const [password, setPassword] = useState('test');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const {store} = useContext(Context);
     //todo: подумать над неймингом 
     const handlerEmail = (e) => {
@@ -15,25 +44,62 @@ const LoginForm = () => {
         setPassword(e.target.value);
     }
     return (
-        <div>
-            <input
-                onChange={handlerEmail}
-                value={email}
-                type='text'
-                placeholder='Email' />
-            <input
-                onChange={handlerPassword}
-                value={password}
-                type='password'
-                placeholder='Password' />
-            <button
-                onClick={() => {
-                    store.login(email, password)
+        <Container maxWidth="xs">
+            <CssBaseline />
+            <Box  
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
                 }}
             >
-                Логин
-            </button>
-        </div>
+                <Avatar sx={{m:1, bgcolor:'secondary.main'}}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Авторизуйся!
+                </Typography>
+            </Box>
+            <Box>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Почта"
+                    name="email"
+                    autoComplete='email'
+                    value={email}
+                    onChange={handlerEmail}
+                />
+                <TextField 
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Пароль"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={handlerPassword}    
+                >
+                </TextField>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    onClick={() => {
+                        store.login(email, password)
+                    }}
+                >
+                     Войти!
+                </Button>
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
     )
 }
 
